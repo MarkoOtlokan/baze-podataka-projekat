ad637aea6c7acf5bad02967f36f77c488fba9a3b
Marko Otlokan <otlokanmarko@gmail.com>
2017-04-23 00:17:52 +0200

    Create klase.py

---
+++ klase.py
@@ -0,0 +1,37 @@
+from peewee import *
+
+db = SqliteDatabase('zadatak.db')
+
+class preduzece(Model):
+    mbpreduzeca=PrimaryKeyField()
+    ime = CharField()
+    vlasnik = CharField()
+    vrednost = CharField()
+    adresa = CharField()
+    brojradnika=CharField()
+    class Meta:
+        database=db
+
+class radnik(Model):
+    mbpreduzeca=ForeignKeyField(preduzece,  db_column='mbpreduzeca')
+    mbradnika=PrimaryKeyField()
+    ime = CharField()
+    prezime = CharField()
+    brojtelefona = CharField()
+    napomena = CharField()
+    plata = CharField()
+    adresa=CharField()
+    godinazaposlenja=CharField()
+    brojprojekata=CharField()
+    godisnjiodmori=CharField()
+    
+    class Meta:
+        database=db
+
+class projekat(Model):
+    mbradnika=ForeignKeyField(radnik, db_column='mbradnika')
+    mbprojekta=CharField()
+    mbsefaprojekta=CharField()
+    vrednostprojekta=CharField()
+    class Meta:
+        database=db

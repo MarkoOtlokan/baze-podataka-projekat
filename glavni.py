0f5db17a26a513da28f2924f01a60f0d83adea1f
Marko Otlokan <otlokanmarko@gmail.com>
2017-04-23 16:35:13 +0200

    Update glavni.py

--- glavni.py
+++ glavni.py
@@ -1,3 +1,4 @@
+
 from klase import *
 import sys
 
@@ -25,12 +26,51 @@ def noviprojekat():
     print 'unesi mbradnika,mbprojekta,mbsefaprojekta,vrednostprojekta'
     mbr,mbp,mbs,vr=raw_input().split()
     pr.create(mbradnika=str(mbr),mbprojekta=str(mbp),mbsefaprojekta=str(mbs),vrednostprojekta=str(vr))
-    
+
+def ispisipreduzeca():
+    print 'ime vlasnik mbpreduzeca vrednost adresa brojradnika'
+    for pred in p.select():
+        print pred.ime, pred.vlasnik,pred.mbpreduzeca,pred.vrednost,pred.adresa,pred.brojradnika
+def ispisiradnike():
+    print 'ime prezime mbr_radnika brojtelefona plata adresa broj projekata napomena'
+    for rad in r.select():
+        print rad.ime,rad.prezime,rad.mbradnika,rad.brojtelefona,rad.plata,rad.adresa,rad.brojprojekata,rad.napomena
+def ispisiprojekte():
+    print 'mbradnika mbprojekta mbsefaprojekta vrednost projekta'
+    for pro in pr.select():
+        print pro.mbprojekta,pro.mbsefaprojekta,pro.vrednostprojekta
+
+def radniciprvefirme(sifre):
+    samooni=r.select().where(r.mbpreduzeca==sifre)
+    for rad in samooni:
+        print rad.ime,rad.prezime,rad.brojtelefona,rad.plata,rad.adresa,rad.brojprojekata,rad.napomena
+def projektiradnika(sifra):
+    samooni=pr.select().where(pr.mbradnika==str(sifra))
+    for pro in samooni:
+        print pro.mbprojekta,pro.mbsefaprojekta,pro.vrednostprojekta
+
+print 'moguce opcije \n ispisi preduzeca \n ispisi radnike \n ispisi projekte \n unesi preduzece \n unesi radnika \n unes projekta \n radnici odredjene firme \n zaduzenje radnika'
 while(1):
     unos=raw_input()
+    if unos=='ispisi preduzeca':
+        ispisipreduzeca()
     if unos=='unesi preduzece':
         novopreduzece()
     if unos=='unesi radnika':
         noviradnik()
     if unos=='unesi projekat':
         noviprojekat()
+    if unos=='ispisi radnike':
+        ispisiradnike()
+    if unos=='ispisi projekte':
+        ispisiprojekte()
+    if unos=='radnici odredjene firme':
+        ispisipreduzeca()
+        print 'unesi mbfirme'
+        sifre=raw_input()
+        radniciprvefirme(sifre)
+    if unos=='zaduzenje radnika':
+        print 'unesi njegov mbr'
+        sifre=raw_input()
+        projektiradnika(sifre)
+    

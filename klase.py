9f568297da41e46e76c9720de53ff9f4736fd931
Marko Otlokan <otlokanmarko@gmail.com>
2017-04-24 23:21:29 +0200

    Update klase.py

--- klase.py
+++ klase.py
@@ -1,30 +1,3 @@
-from peewee import *
-
-db = SqliteDatabase('zadatak.db')
-
-class preduzece(Model):
-    mbpreduzeca=PrimaryKeyField()
-    ime = CharField()
-    vlasnik = CharField()
-    vrednost = CharField()
-    adresa = CharField()
-    brojradnika=CharField()
-    class Meta:
-        database=db
-
-class radnik(Model):
-    mbpreduzeca=ForeignKeyField(preduzece,  db_column='mbpreduzeca')
-    mbradnika=PrimaryKeyField()
-    ime = CharField()
-    prezime = CharField()
-    brojtelefona = CharField()
-    napomena = CharField()
-    plata = CharField()
-    adresa=CharField()
-    godinazaposlenja=CharField()
-    brojprojekata=CharField()
-    godisnjiodmori=CharField()
-    
     class Meta:
         database=db
 
@@ -34,4 +7,20 @@ class projekat(Model):
     mbsefaprojekta=CharField()
     vrednostprojekta=CharField()
     class Meta:
-        database=db
+        database=db                                                                                                                                                 
+
+class imovinafirme(Model):
+    mbpreduzeca=ForeignKeyField(preduzece,  db_column='mbpreduzeca')
+    vrednost=CharField()
+    naziv=CharField()
+
+class javnenabavke(Model):
+    mbpreduzeca=ForeignKeyField(preduzece,  db_column='mbpreduzeca')
+    vrednost=CharField()
+    naziv=CharField()
+    datum=CharField()
+
+class imovinaradnika(Model):
+    mbpreduzeca=ForeignKeyField(preduzece,  db_column='mbpreduzeca')
+    vrednost=CharField()
+    naziv=CharField()
